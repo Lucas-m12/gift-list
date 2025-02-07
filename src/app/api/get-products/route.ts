7448ea0d539e0d44459eb8dc852d3512c746af22
Lucas Marinho <lucasmarinhodasilva2@gmail.com>
2025-02-06 21:56:00 -0300

    update query

--- src/app/api/get-products/route.ts
+++ src/app/api/get-products/route.ts
@@ -1,18 +1,27 @@
 import { prisma } from "@/prisma-client";
 
 export const GET = async () => {
-  const products = await prisma.products.findMany({
-    include: {
-      categories: true,
-    },
-    orderBy: {
-      shouldRemove: "asc",
-      name: "asc"
-    }
-  });
-
-  return Response.json(products.map(product => ({
-    ...product,
-    categories: product.categories.map(category => category.id),
-  })));
+  try {
+    const products = await prisma.products.findMany({
+      include: {
+        categories: true,
+      },
+      orderBy: [
+        {
+          shouldRemove: "asc",
+        },
+        {
+          name: "asc"
+        }
+      ]
+    });
+  
+    return Response.json(products.map(product => ({
+      ...product,
+      categories: product.categories.map(category => category.id),
+    })));
+  } catch (error) {
+    console.log(error);
+    return Response.json([]);
+  }
 }
